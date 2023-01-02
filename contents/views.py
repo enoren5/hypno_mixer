87e7744dd6b49a9f66ac9452f7b2bb650ca05c9e
Pete Naveen Pearson <warden496@protonmail.com>
2023-01-02 06:28:37 -0500

    Sort by LogEntry cleanup for Django forums

--- contents/views.py
+++ contents/views.py
@@ -11,6 +11,77 @@ from django.contrib.contenttypes.models import ContentType
 from django.db.models.functions import Cast
 from django.db.models import CharField
 
+class ContentListView(LoginRequiredMixin,ListView):
+    model = Content
+    # template_name = 'home.html'   
+        
+    def get_context_data(self, **kwargs):
+    
+        # Call the base implementation first to get a context
+        context = super(ContentListView, self).get_context_data(**kwargs)
+        script_type = ContentType.objects.get_for_model(Research)
+        script_type = ContentType.objects.get_for_model(ScriptSuggestion)
+    
+        context['inductions'] = Induction.objects.all()
+        context['preambles'] = Preamble.objects.all()
+        context['scriptsuggestions'] = ScriptSuggestion.objects.annotate(
+            last_change=Subquery(
+                LogEntry.objects.filter(
+                    content_type=script_type,
+                    action_flag=CHANGE,
+                    object_id=Cast(
+                        OuterRef('id'), 
+                        CharField()
+                        )
+                    ).order_by('-action_time').values('action_time')[:1]
+                )
+            ).order_by('-last_change')
+        context['stockscripts'] = StockScript.objects.all()
+        context['research'] = Research.objects.annotate(
+            last_change=Subquery(
+                LogEntry.objects.filter(
+                    content_type=script_type,
+                    action_flag=CHANGE,
+                    object_id=Cast(
+                        OuterRef('id'), 
+                        CharField()
+                        )
+                    ).order_by('-action_time').values('action_time')[:1]
+                )
+            ).order_by('-last_change')
+        return context
+
+class PreambleDetailView(LoginRequiredMixin,DetailView):
+    model = Preamble
+    context_object_name = 'preambles'
+
+class InductionDetailView(LoginRequiredMixin, DetailView):
+    model = Induction
+    context_object_name = 'inductions'
+    
+class ScriptSuggestionDetailView(LoginRequiredMixin,DetailView):
+    model = ScriptSuggestion        
+    context_object_name = 'scriptsuggestions'
+
+class StockScriptDetailView(LoginRequiredMixin,DetailView):
+    model = StockScript
+    context_object_name = 'stockscripts'
+
+
+class ResearchDetailView(LoginRequiredMixin, DetailView):
+    model = Research    
+    context_object_name = 'research'
+        
+'''def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(ContentDetailView, self).get_context_data(**kwargs)
+        # Add in a QuerySet of all the Baklawa
+        context['preambles'] = Preamble.objects.all()
+        #context['research'] = Research.objects.all()
+        context['scriptsuggestions'] = ScriptSuggestion.objects.all()
+        context['stockscripts'] = StockScript.objects.all()
+        return context'''
+        
 '''
 class CustomLoginView(LoginView):
     form_class = LoginForm
@@ -24,87 +95,3 @@ class CustomLoginView(LoginView):
 def index(request):
     return render(request,'contents/index.html')
 '''
-
-class ContentListView(LoginRequiredMixin,ListView):
-    # model_list.html
-    # model = Induction
-    model = Content
-    # login_url = '/'
-    # template_name = 'home.html'
-    
-    # fields =['author','title',]
-    
-    def get_context_data(self, **kwargs):
-        # Call the base implementation first to get a context
-        context = super(ContentListView, self).get_context_data(**kwargs)
-        # Add in a QuerySet of all the Baklawa
-        context['inductions'] = Induction.objects.all()
-        context['preambles'] = Preamble.objects.all()
-        context['research'] = Research.objects.all()
-        context['scriptsuggestions'] = ScriptSuggestion.objects.all()
-
-        context['stockscripts'] = StockScript.objects.all()
-        return context
-
-class PreambleDetailView(LoginRequiredMixin,DetailView):
-    model = Preamble
-    context_object_name = 'preambles'
-
-class InductionDetailView(LoginRequiredMixin, DetailView):
-    model = Induction
-    context_object_name = 'inductions'
-    
-class ScriptSuggestionDetailView(LoginRequiredMixin,DetailView):
-    model = ScriptSuggestion
-    
-    script_type = ContentType.objects.get_for_model(ScriptSuggestion)
-    
-    ScriptSuggestion.objects.annotate(
-        last_change=Subquery(
-            LogEntry.objects.filter(
-                content_type=script_type,
-                action_flag=CHANGE,
-                object_id=Cast(
-                    OuterRef('id'), 
-                    CharField()
-                    )
-            ).order_by('-action_time').values('action_time')[:1]
-        )
-    ).order_by('-last_change')
-        
-    context_object_name = 'scriptsuggestions'
-
-class StockScriptDetailView(LoginRequiredMixin,DetailView):
-    model = StockScript
-    context_object_name = 'stockscripts'
-
-
-class ResearchDetailView(LoginRequiredMixin, DetailView):
-    model = Research
-        
-    script_type = ContentType.objects.get_for_model(Research)
-    
-    Research.objects.annotate(
-        last_change=Subquery(
-            LogEntry.objects.filter(
-                content_type=script_type,
-                action_flag=CHANGE,
-                object_id=Cast(
-                    OuterRef('id'), 
-                    CharField()
-                    )
-            ).order_by('-action_time').values('action_time')[:1]
-        )
-    ).order_by('-last_change')
-    
-    context_object_name = 'research'
-        
-'''def get_context_data(self, **kwargs):
-        # Call the base implementation first to get a context
-        context = super(ContentDetailView, self).get_context_data(**kwargs)
-        # Add in a QuerySet of all the Baklawa
-        context['preambles'] = Preamble.objects.all()
-        #context['research'] = Research.objects.all()
-        context['scriptsuggestions'] = ScriptSuggestion.objects.all()
-        context['stockscripts'] = StockScript.objects.all()
-        return context'''
