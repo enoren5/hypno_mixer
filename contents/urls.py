d7440d9bb6dbd3eed20255d1deaa98fc3b15752b
Pete Naveen Pearson <drone4four@gmail.com>
2026-01-16 23:30:33 -0500

    best-practices: Add trailing `/` to patterm urls

--- contents/urls.py
+++ contents/urls.py
@@ -6,14 +6,14 @@ app_name = 'contents'
 urlpatterns = [
     path('', ContentListView.as_view(),name='home'), 
     #path('', AssortedPeriodicalsListView.as_view(),name='assorted_periodicals'), 
-    path('preambles/<str:slug>', PreambleDetailView.as_view(),name='preamble_details'), 
-    path('inductioning/<str:slug>', InductionDetailView.as_view(),name='induction_details'), 
-    path('scripting/<str:slug>', ScriptSuggestionDetailView.as_view(),name='scriptsuggestion_detail'), 
-    path('stock_scripting/<str:slug>', StockScriptDetailView.as_view(),name='stockscript_detail'), 
-    path('researching/<str:slug>', ResearchDetailView.as_view(),name='research_detail'), 
-    path('nytimesing/<str:slug>', NYTimesDetailView.as_view(),name='nytimes_detail'), 
-    path('torstaring/<str:slug>', TorStarDetailView.as_view(),name='torstar_detail'), 
-    path('wsj-ing/<str:slug>', WSJournalDetailView.as_view(),name='wsj_detail'), 
+    path('preambles/<str:slug>/', PreambleDetailView.as_view(),name='preamble_details'), 
+    path('inductioning/<str:slug>/', InductionDetailView.as_view(),name='induction_details'), 
+    path('scripting/<str:slug>/', ScriptSuggestionDetailView.as_view(),name='scriptsuggestion_detail'), 
+    path('stock_scripting/<str:slug>/', StockScriptDetailView.as_view(),name='stockscript_detail'), 
+    path('researching/<str:slug>/', ResearchDetailView.as_view(),name='research_detail'), 
+    path('nytimesing/<str:slug>/', NYTimesDetailView.as_view(),name='nytimes_detail'), 
+    path('torstaring/<str:slug>/', TorStarDetailView.as_view(),name='torstar_detail'), 
+    path('wsj-ing/<str:slug>/', WSJournalDetailView.as_view(),name='wsj_detail'), 
 ]
 
     
