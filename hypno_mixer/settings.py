994e7b98197c4ea914a4ad81dd5a7dcf2edd70ae
Pete Naveen Pearson <drone4four@gmail.com>
2026-01-16 22:35:22 -0500

    Add dyanamic DB reference for PostgreSQL

--- hypno_mixer/settings.py
+++ hypno_mixer/settings.py
@@ -96,10 +96,11 @@ WSGI_APPLICATION = 'hypno_mixer.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(BASE_DIR / 'db.sqlite3'),
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
 
 
