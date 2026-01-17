d46847ec8606f1f873c7a0574b799b893e88bb8f
Pete Naveen Pearson <drone4four@gmail.com>
2026-01-16 22:22:28 -0500

    Correct static dir for Heroku

--- hypno_mixer/settings.py
+++ hypno_mixer/settings.py
@@ -169,7 +169,7 @@ Trying this instead :
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [ os.path.join(BASE_DIR,'static') ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # I got the below from:
